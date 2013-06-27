56f96ae49d261065c935e015a2eb52d11f7564bb
qianbenben1@googlemail.com <qianbenben1@googlemail.com@fb079ee1-1838-2000-ec0b-3dbaca26b09f>
2013-06-27 21:35:33 +0000

    change form item name from "word1" to "data"
    git-svn-id: https://word-guess.googlecode.com/svn/trunk@15 fb079ee1-1838-2000-ec0b-3dbaca26b09f

--- test/project_v0.1/app/forms.py
+++ test/project_v0.1/app/forms.py
@@ -7,4 +7,4 @@ class LoginForm(Form):
 
 class FirstForm(Form):
     name = TextField('name', validators = [Required()])
-    word1 = TextField('word1', validators = [Required()])
+    data = TextField('data', validators = [Required()])
