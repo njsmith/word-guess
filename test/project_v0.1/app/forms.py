c7f81aa642297c0d370f1689135db761f08accc4
qianbenben1@googlemail.com <qianbenben1@googlemail.com@fb079ee1-1838-2000-ec0b-3dbaca26b09f>
2013-07-02 13:19:56 +0000

    Clean up some codes
    git-svn-id: https://word-guess.googlecode.com/svn/trunk@25 fb079ee1-1838-2000-ec0b-3dbaca26b09f

--- test/project_v0.1/app/forms.py
+++ test/project_v0.1/app/forms.py
@@ -1,12 +1,6 @@
 from flask.ext.wtf import Form, TextField, BooleanField
 from flask.ext.wtf import Required
 
-class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
-    remember_me = BooleanField('remember_me', default = False)
-
 class FirstForm(Form):
-    language = TextField('language', validators = [Required()])
     main_data = TextField('main_data', validators = [Required()])
-    age = TextField('age', validators = [Required()])
     text_index = TextField('age', validators = [Required()])
--- test/project_v0.1/app/models.py
+++ test/project_v0.1/app/models.py
@@ -5,39 +5,5 @@ ROLE_ADMIN = 1
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    nickname = db.Column(db.String(64), index = True, unique = False)
-    email = db.Column(db.String(120), index = True, unique = False)
-    role = db.Column(db.SmallInteger, default = ROLE_USER)
-    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
     text_index = db.Column(db.String(1024), index = True, unique = False)
-    language = db.Column(db.String(1024), index = True, unique = False)
-    age = db.Column(db.String(1024), index = True, unique = False)
     main_data = db.Column(db.String(10240), index = True, unique = False)
-   
-
-    def __repr__(self):
-        return '<User %r>' % (self.nickname)
-
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return unicode(self.id)
-
-    def __repr__(self):
-        return '<User %r>' % (self.nickname)
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Post %r>' % (self.body)
