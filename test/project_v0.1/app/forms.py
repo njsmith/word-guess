8d3677212f129865e916f9daf6da19a270cc8400
qianbenben1@googlemail.com <qianbenben1@googlemail.com@fb079ee1-1838-2000-ec0b-3dbaca26b09f>
2013-07-08 19:17:55 +0000

    git-svn-id: https://word-guess.googlecode.com/svn/trunk@36 fb079ee1-1838-2000-ec0b-3dbaca26b09f

--- test/project_v0.1/app/forms.py
+++ test/project_v0.1/app/forms.py
@@ -3,4 +3,4 @@ from flask.ext.wtf import Required
 
 class FirstForm(Form):
     main_data = TextField('main_data', validators = [Required()])
-    text_index = TextField('age', validators = [Required()])
+    text_index = TextField('text_index', validators = [Required()])
