deb6d56ccaa72bba6ece9122fa3a321e56cf5dce
qianbenben1@googlemail.com <qianbenben1@googlemail.com@fb079ee1-1838-2000-ec0b-3dbaca26b09f>
2013-07-21 18:14:42 +0000

    !!!Pending!!!
    git-svn-id: https://word-guess.googlecode.com/svn/trunk@48 fb079ee1-1838-2000-ec0b-3dbaca26b09f

--- test/project_v0.1/app/models.py
+++ test/project_v0.1/app/models.py
@@ -1,12 +1,31 @@
 from app import db
+import time
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.String, primary_key = True)
     worker_id = db.Column(db.String, index = True, unique = False)
     text_index = db.Column(db.String, index = True, unique = False)
     start_position = db.Column(db.String, index = True, unique = False)
     gap = db.Column(db.String, index = True, unique = False)
     main_data = db.Column(db.String, index = True, unique = False)
+
+class Pending(db.Model):
+    id = db.Column(db.String, primary_key = True)
+    worker_id = db.Column(db.String, index = True, unique = False)
+    text_index = db.Column(db.String, index = True, unique = False)
+    start_position = db.Column(db.String, index = True, unique = False)
+    gap = db.Column(db.String, index = True, unique = False)
+    start_time = db.Column(db.Float)
+
+    @classmethod
+    def expire(cls, session):
+        now = time.time()
+        duration_hours = 1
+        last_good_start_time = now - ( duration_hours * 60 * 60 )
+        for hit in (cls.query
+                     .filter(cls.start_time < last_good_start_time)):
+            session.delete(hit)
+        session.commit()
--- test/project_v0.1/app/views.py
+++ test/project_v0.1/app/views.py
@@ -1,11 +1,12 @@
 from flask import render_template, flash, redirect, session, url_for, request, g, send_file
 from app import app, db, lm, oid
 from forms import FirstForm
-from models import User, ROLE_USER, ROLE_ADMIN
+from models import User, Pending, ROLE_USER, ROLE_ADMIN
 
 from texts import text_dict, title_dict
 import time
 import random
+import uuid
 
 # index view function suppressed for brevity
 
@@ -23,17 +24,23 @@ def FillFirstForm():
         getText_index = request.form["text_index"]
         getStart_position = request.form["start_position"]
         getGap = request.form["gap"]
-        #getWorker_id = form.worker_id.data
-        #getData = form.main_data.data
-        #getText_index = form.text_index.data
-        #getStart_position = form.start_position.data
-        #getGap = form.gap.data
-
-        user = User(worker_id = getWorker_id, text_index = getText_index, start_position = getStart_position, gap = getGap, main_data = getData)
+        getHit_id = request.form["hit_id"]
+        
+        # Add finished data to DB
+        user = User(id = getHit_id, worker_id = getWorker_id, text_index = getText_index, start_position = getStart_position, gap = getGap, main_data = getData)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('ThankYou'))
 
+        # Delete same ID in Pending DB
+        for hit in (Pending.query
+                     .filter_by(id = getHit_id)):
+            db.session.delete(hit)
+        db.session.commit()
+
+        return redirect(url_for('ThankYou'))
+    
+    # Check expired pending
+    Pending.expire(db.session)
     if request.args["assignmentId"] == "ASSIGNMENT_ID_NOT_AVAILABLE":
         is_preview = True
     else:
@@ -45,6 +52,7 @@ def FillFirstForm():
         
     # Any text have not done by this worker
     available_text = text_dict.keys()
+
     have_done = User.query.filter_by(worker_id = worker_id)
     for hit in have_done:
         removed = int(hit.text_index)
@@ -56,22 +64,26 @@ def FillFirstForm():
     # Check the least text number
     temp_least = 99999
     need_remove = []
+    # Find out the least done number of texts
     for count in available_text:
-        current_count = User.query.filter_by(text_index = count).count()
+        current_count = User.query.filter_by(text_index = count).count() + Pending.query.filter_by(text_index = count).count()
         if current_count < temp_least:
             temp_least = current_count
+    # If the done number of the text is larger than the least done number
+    # Put this text into need_remove list
     for count in available_text:
-        current_count = User.query.filter_by(text_index = count).count()
+        current_count = User.query.filter_by(text_index = count).count() + Pending.query.filter_by(text_index = count).count()
         if current_count > temp_least:
             need_remove.append(count)
+    # Remove illegal text from available_text list
     available_text = list(set(available_text)-set(need_remove)) 
-    
 
     # Gap
     gap = 300
     # Start position
     start = random.randint(1, 1+gap)
-    #start = 1
+    # Generate a ID for this HIT
+    hit_id = uuid.uuid4().hex
    
     # Text
     select_text_index = random.choice(available_text)
@@ -84,6 +96,12 @@ def FillFirstForm():
         paraglen[parag] = len(select_text[parag])
         print paraglen[parag]
 
+    #When sent user a text
+    #save a copy to Pending as well
+    pending = Pending(id = hit_id, worker_id = worker_id, text_index = select_text_index, start_position = start, gap = gap, start_time = time.time())
+    db.session.add(pending)
+    db.session.commit()
+
     return render_template('FirstForm.html', 
         title = 'First Form',
         form = form,
@@ -95,7 +113,8 @@ def FillFirstForm():
         gap = gap,
         select_text_index = select_text_index,
         is_preview = is_preview,
-        worker_id = worker_id
+        worker_id = worker_id,
+        hit_id = hit_id
         )
 
 @app.route('/ThankYou')
