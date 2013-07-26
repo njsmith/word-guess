be02e127dc814667d7eb7b440c571d041ad7692b
njs@vorpus.org <njs@vorpus.org@fb079ee1-1838-2000-ec0b-3dbaca26b09f>
2013-07-26 15:30:22 +0000

    update turk-status.py script
    git-svn-id: https://word-guess.googlecode.com/svn/trunk@55 fb079ee1-1838-2000-ec0b-3dbaca26b09f

--- test/project_v0.1/turk-status.py
+++ test/project_v0.1/turk-status.py
@@ -18,18 +18,27 @@ mtc = MTurkConnection(aws_access_key_id=aws_access_key_id,
                       aws_secret_access_key=aws_secret_access_key,
                       host=HOST)
 
-HITs = []
-for HIT in mtc.get_all_hits():
-    if title_re.search(HIT.Title):
-        HITs.append(HIT)
-unexpired = 0
+def get_matching_HITs(mtc, regex):
+    HITs = []
+    for HIT in mtc.get_all_hits():
+        if title_re.search(HIT.Title):
+            HITs.append(HIT)
+    return HITs
+
+if len(sys.argv) > 1 and sys.argv[1] == "--expire-all":
+    print("Expiring all outstanding HITs...")
+    for HIT in get_matching_HITs(mtc, title_re):
+        mtc.expire_hit(HIT.HITId)
+    print("...done")
+# Re-fetch after expiring, so as to make sure we have up-to-date information.
+HITs = get_matching_HITs(mtc, title_re)
+live = 0
 bad_workers = set()
 need_reviewed = set()
 total_cost = 0
 pending_cost = 0
+max_future_cost = 0
 for HIT in HITs:
-    if not HIT.expired:
-        unexpired += 1
     assignments = mtc.get_assignments(HIT.HITId, page_size=50)
     approved = 0
     pending = 0
@@ -48,18 +57,27 @@ for HIT in HITs:
     hit_cost_pending = pending * reward
     total_cost += hit_cost_so_far
     pending_cost += hit_cost_pending
+    # I *think* you add these together to get the maximum number of
+    # assignments that might be submitted, but I'm not 100% sure.
+    max_future_assignments = (int(HIT.NumberOfAssignmentsAvailable) +
+                              int(HIT.NumberOfAssignmentsPending))
+    if not HIT.expired and max_future_assignments > 0:
+        live += 1
+        max_future_cost += max_future_assignments * reward
     sys.stdout.write("%s: %s @ %0.2f = $%0.2f"
                      % (HIT.HITId, approved, reward, hit_cost_so_far))
     if pending:
         sys.stdout.write(" (+ %s @ %0.2f = $%0.2f)"
                          % (pending, reward, hit_cost_pending))
     sys.stdout.write("\n")
-print("Total nominal cost: %0.2f" % (total_cost,))
 # 10% Amazon commission. NB: this assumes that all our rewards are >= $0.05,
 # and that we aren't using "master" workers:
-print("Total actual cost: %0.3f" % (total_cost * 1.10))
-print("Total nominal cost, with pending: %0.2f" % (total_cost + pending_cost,))
-print("Total actual cost, with pending: %0.3f" % (1.10 * (total_cost + pending_cost),))
+print("Total paid: %0.2f + 10%% = $%0.3f" % (total_cost, total_cost * 1.10))
+print("Total paid + pending: %0.2f + 10%% = $%0.3f"
+      % (total_cost + pending_cost, 1.10 * (total_cost + pending_cost)))
+print("Maximum future cost: %0.2f + 10%% = $%0.3f"
+      % (total_cost + pending_cost + max_future_cost,
+         1.10 * (total_cost + pending_cost + max_future_cost)))
 print("")
 print("Bad workers:")
 for worker in bad_workers:
@@ -72,10 +90,8 @@ if need_reviewed:
               % (hit_id,))
 
 print("")
-print("HITs that are not expired: %s" % (unexpired,))
-if len(sys.argv) > 1 and sys.argv[1] == "--expire-all":
-    for HIT in HITs:
-        mtc.expire_hit(HIT.HITId)
-    print("  ...but I just expired them, so never mind!")
-else:
+if live:
+    print("HITs that are live: %s" % (live,))
     print("  (Use --expire-all to mark them expired now)")
+else:
+    print("No HITs are currently live.")
